c9ad6f03718dd87865d6dd440d7eef2565bcc7f8
afterforever17 <pbggabi@gmail.com>
2025-11-02 22:25:16 +0200

    Add Services

---
+++ src/main/java/vcarb/stockexchange/server/services/StockService.java
@@ -0,0 +1,43 @@
+package vcarb.stockexchange.server.services;
+
+import vcarb.stockexchange.server.entities.Stock;
+import vcarb.stockexchange.server.repositories.StockRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class StockService {
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    public List<Stock> getAllStocks(){
+        return stockRepository.findAll();
+    }
+
+    public Optional<Stock> getStockById(Long id){
+        return stockRepository.findById(id);
+    }
+
+    public Stock createStock(Stock stock){
+        return stockRepository.save(stock);
+    }
+
+    public Stock updateStock(Long id, Stock newStock){
+        return stockRepository.findById(id)
+                .map(stock ->  {
+                    stock.setName(newStock.getName());
+                    stock.setAmount(newStock.getAmount());
+                    stock.setPrice(newStock.getPrice());
+                    stock.setAprecCoef(newStock.getAprecCoef());
+                    return stockRepository.save(stock);
+                })
+                .orElseThrow(() -> new RuntimeException("Stock doesn't exist"));
+    }
+
+    public void deleteStock(Long id){
+        stockRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/vcarb/stockexchange/server/services/TransactionService.java
@@ -0,0 +1,38 @@
+package vcarb.stockexchange.server.services;
+
+import vcarb.stockexchange.server.entities.Stock;
+import vcarb.stockexchange.server.entities.Transaction;
+import vcarb.stockexchange.server.repositories.StockRepository;
+import vcarb.stockexchange.server.repositories.TransactionRepository;
+
+import java.util.List;
+
+public class TransactionService {
+    private final TransactionRepository transactionRepository;
+    private final StockRepository stockRepository;
+
+    public TransactionService(TransactionRepository transactionRepository, StockRepository stockRepository) {
+        this.transactionRepository = transactionRepository;
+        this.stockRepository = stockRepository;
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public List<Transaction> getTransactionsByStockId(Long stockId) {
+        return transactionRepository.findByStockId(stockId);
+    }
+
+    public Transaction createTransaction(Transaction transaction) {
+        Stock stock = stockRepository.findById(transaction.getStock().getId())
+                .orElseThrow(() -> new RuntimeException("Stock doesn't exist"));
+
+        transaction.setStock(stock);
+        return transactionRepository.save(transaction);
+    }
+
+    public void deleteTransaction(Long transactionId) {
+        transactionRepository.deleteById(transactionId);
+    }
+}
