7c01e74479633328721220a815f21cdd368bb700
Lintes Claudiu <claudiu.lintes@student.upt.ro>
2025-11-02 23:20:49 +0200

    Created Transaction Controller

--- src/main/java/vcarb/stockexchange/server/controllers/TransactionController.java
+++ src/main/java/vcarb/stockexchange/server/controllers/TransactionController.java
@@ -1,29 +1,49 @@
 package vcarb.stockexchange.server.controllers;
 
 import io.swagger.v3.core.util.Json;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import vcarb.stockexchange.server.MainController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import vcarb.stockexchange.server.entities.TransactionEntity;
+import vcarb.stockexchange.server.repositories.TransactionRepository;
+
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/transactions")
 public class TransactionController {
-
- /*   // ===== Transactions =====
-
-    @GetMapping("/transaction/all")
-    public String getAllTransaction(){
-        setAll();
-        List<MainController.Transaction> response = transactionsList;
-        return Json.pretty(response);
+    public final TransactionRepository transactionRepository;
+    public TransactionController(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
     }
 
-    @GetMapping("transaction/{transactionID}")
-    public String getUserTransaction(@PathVariable Long transactionID){
-        setAll();
-        MainController.Transaction response = getById(transactionsList,transactionID, MainController.Transaction::getTransactionId);
-        return Json.pretty(response);
-    }*/
+    @GetMapping("/getAll")
+    public List<TransactionEntity> getAllTransaction()
+    {
+        return transactionRepository.findAll();
+    }
+
+    @GetMapping("/getTransactionById/{transactionID}")
+    public ResponseEntity<TransactionEntity> getTransaction(@PathVariable long transactionID){
+        return transactionRepository.findById(transactionID).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/createTransaction")
+    public TransactionEntity createTransaction(@RequestBody TransactionEntity transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    @PutMapping("updateTransactionById/{transactionId}")
+    public ResponseEntity<TransactionEntity> updateTransaction(@PathVariable Long transactionId, @RequestBody TransactionEntity updatedTransaction) {
+        return null;
+    }
+
+    @DeleteMapping("deleteTransactionById/{transactionId}")
+    public ResponseEntity<Void> deleteTransaction(@PathVariable Long transactionId) {
+        if (transactionRepository.existsById(transactionId)) {
+            transactionRepository.deleteById(transactionId);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/vcarb/stockexchange/server/repositories/TransactionRepository.java
+++ src/main/java/vcarb/stockexchange/server/repositories/TransactionRepository.java
@@ -1,4 +1,8 @@
 package vcarb.stockexchange.server.repositories;
 
-public interface TransactionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import vcarb.stockexchange.server.entities.TransactionEntity;
+
+public interface TransactionRepository extends JpaRepository<TransactionEntity,Long> {
+
 }
